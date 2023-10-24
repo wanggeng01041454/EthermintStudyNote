8c7d71a10cd779a690e73bd1a4e2c0174bd37cba
wanggeng <wanggeng01041454@163.com>
2023-10-24 10:32:17 +0800

    完成了 ABCI 分析文章的输出。 调整了总体架构中的分阶段图。

--- env-scripts/solo_mode/solo_export_private_key.sh
+++ env-scripts/solo_mode/solo_export_private_key.sh
@@ -1,1 +1,10 @@
-y
+#!/bin/bash
+
+WORK_DIR=/tmp/solo_dir
+KEY=solo_mykey
+KEYRING=test
+MONIKER=solo_test
+
+# 使用管道，提前把交互命令准备好
+echo "y" | ethermintd keys export ${KEY} --keyring-backend ${KEYRING} --unsafe --unarmored-hex  --home ${WORK_DIR}
+
